44209669dd3d26911543832d3b8d0ba5bf6b12b7
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-04-01 21:04:31 -0400

    Add files via upload

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -52,3 +52,57 @@ def other_exp(base, pow)
 
 end
 
+def deep_dup(array, new_array = [])
+  return if array == []
+
+  if array[0].is_a?(Array)
+    subArray = []
+    array[0].each do |ele|
+      dupEle = ele
+      subArray << dupEle
+    end
+    new_array << subArray
+  else 
+    dupEle = array[0]
+    new_array << dupEle
+  end
+  
+  deep_dup(array[1..-1], new_array)
+
+  return new_array
+end
+
+#print deep_dup([["nuts", "bolts", "washers"],["capacitors", "resistors", "inductors"]])
+#print deep_dup([1, [2], [3, [4]]])
+
+#robot_parts = [["nuts", "bolts", "washers"], ["capacitors", "resistors", "inductors"]]
+#robot_parts_copy = deep_dup(robot_parts)
+#robot_parts_copy[1] << "LEDs"
+
+#puts robot_parts[1] # => ["capacitors", "resistors", "inductors", "LEDs"]
+#puts robot_parts_copy[1]
+
+#the fibonacci sequence is a sequence of numbers where the number is the addition of the previous two numbers
+#starting at 0, 1
+# 0, 1, 1, 2, 3, 5, 8, 13, 21
+def fibonacci(n)
+  return [] if n == 0
+  return [0] if n == 1
+  return [0, 1] if n == 2
+
+  fib = fibonacci(n - 1)
+
+  fib << fib[-2] + fib[-1]
+
+  return fib
+end
+
+def iter_fibonacci(n)
+  ans = [0, 1]
+
+  (2..n).each do |num|
+    ans << ans[num - 2] + ans[num - 1]
+  end
+
+  return ans
+end
